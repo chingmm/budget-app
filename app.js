f946101dd9e5843607c28af6ff066e95c8c72962
chingmm <chingmm19@gmail.com>
2020-08-26 20:11:07 -0500

    event handler for form not working

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log("Hello")
+
+// const transactions = []
+
+// let incomeAmount = 0
+
+// console.log($("#income-amount").val())
+
+// $("#income-amount").on("submit", (event) => {
+//     event.preventDefault()
+
+//     incomeAmount = $("#income-amount").val()
+// })
+
+// console.log(incomeAmount)
+
+const incomeSubmit = (event) => {
+    console.log(event)
+    event.preventDefault()
+}
+
+$('#income').on('submit', incomeSubmit())
